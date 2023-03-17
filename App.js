11da235e5a5e5bf635aa988288c2be044aea5b67
Vaishak Kumar N P <vaishakmanoj96@gmail.com>
2023-03-17 18:37:29 +0530

    babel_tree shaking_npx

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
-const heading = React.createElement("h1", {id: "title",}, "hello")
+import React from "react" ;
+import ReactDOM from "react-dom/client" ;
 
+
+
+const heading = React.createElement("h1", {
+    id: "title",
+}, "hello my brother ")
 console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(heading);
